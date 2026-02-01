dc21696d72c445605ceb3f9637aaabf28f999643
dasbastardo <aduhbgst5@gmail.com>
2026-02-02 00:31:39 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../pages/style.css";
 
 export default function Home({ serverTime }) {
   return (
